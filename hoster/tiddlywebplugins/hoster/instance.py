a10f1d67c0857a3818d033510e4ee47296f10c85
Chris Dent <chris.dent@gmail.com>
2009-12-17 20:50:28 +0000

    more cleanly merge the the tiddlywebwiki instance data with the hoster instance data.

--- hoster/tiddlywebplugins/hoster/instance.py
+++ hoster/tiddlywebplugins/hoster/instance.py
@@ -1,30 +1,20 @@
 
 from tiddlywebplugins.instancer.util import get_tiddler_locations
-from tiddlywebwiki.instance import (
-        store_contents as tww_store_contents,
-        store_structure as tww_store_structure)
+from tiddlywebwiki.instance import store_contents, store_structure
 from tiddlywebwiki.config import config
 
-store_contents = {
-        'hoster': [
-            'file:basecontent/main.css.tid',
-            ]
-        }
+store_contents['hoster'] = ['file:basecontent/main.css.tid']
 
-store_structure = {
-        'bags': {
-            'hoster': {
-                'desc': 'useful stuff that hoster wants to use',
-                'policy': {
-                    'read': '',
-                    'write': 'R:ADMIN',
-                    'create': 'R:ADMIN',
-                    'delete': 'R:ADMIN',
-                    'manage': 'R:ADMIN',
-                    'owner': 'administrator',
-                    }
-                },
-            },
+store_structure['bags']['hoster'] = {
+        'desc': 'useful stuff that hoster wants to use',
+        'policy': {
+            'read': [],
+            'write': ['R:ADMIN'],
+            'create': ['R:ADMIN'],
+            'delete': ['R:ADMIN'],
+            'manage': ['R:ADMIN'],
+            'owner': 'administrator',
+            }
         }
 
 instance_config = {
@@ -34,8 +24,5 @@ instance_config = {
         'register.start_href': '/home',
         }
 
-instance_tiddlers = get_tiddler_locations(store_contents, 'tiddlywebplugins.hoster')
-store_contents.update(tww_store_contents)
-store_structure.update(tww_store_structure)
-instance_tiddlers.update(config['instance_tiddlers'])
-
+instance_tiddlers = get_tiddler_locations(store_contents,
+        'tiddlywebplugins.hoster')
