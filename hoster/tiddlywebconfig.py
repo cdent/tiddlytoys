d23d3a5168caa57870b38e8983ec29730c800c81
Chris Dent <cdent@oxide.local>
2010-01-02 19:34:52 +0000

    clean up

--- hoster/tiddlywebconfig.py
+++
@@ -1,7 +0,0 @@
-import mangler
-config = {
-        'log_level': 'DEBUG',
-        'system_plugins': ['logout', 'tiddlywebplugins.hoster'],
-        #'twanager_plugins': ['twedit', 'tiddlywebplugins.hoster'],
-        'twanager_plugins': ['tiddlywebplugins.hoster'],
-        }
