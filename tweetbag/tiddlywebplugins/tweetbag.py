a816b6edce3542919e57e7433a3534912fadce3d
Chris Dent <cdent@oxide.local>
2010-01-07 22:21:25 +0000

    Start migrating to putting the service providers information in a tiddler.

--- tweetbag/tiddlywebplugins/tweetbag.py
+++ tweetbag/tiddlywebplugins/tweetbag.py
@@ -1,8 +1,9 @@
 """
 """
-import oauth
 import urllib2
 import simplejson
+
+from oauth import oauth
 
 from tiddlyweb.model.tiddler import Tiddler
 from tiddlyweb.web.http import HTTP302, HTTP303, HTTP400
@@ -23,19 +24,8 @@ OAUTH_BAG_POLICY = {
         'accept': ["NONE"],
         }
 OAUTH_BAG_OWNER = 'administrator'
-SERVICE_NAME = 'twitter'
 
-class twitter:
-    # XXX this information should come from a tiddler
-    # probably one called service_<something>
-    # the key and secret should be unique to any given installation
-    # where possible
-    CONSUMER_KEY = 'ux6RvTyvlK6BtGTcrw7KfQ'
-    CONSUMER_SECRET = 'JxKoTVwITcZjoA5fzxdJWHJJ9cSCteBNrOti35xQblA'
-    REQUEST_TOKEN_URL = 'http://twitter.com/oauth/request_token'
-    ACCESS_TOKEN_URL  = 'http://twitter.com/oauth/access_token'
-    AUTHORIZATION_URL = 'http://twitter.com/oauth/authorize'
-
+TWITTER_GET = 'http://hoster.peermore.com/bags/hoster/tiddlers/bagdesc.js'
 
 def init(config):
     if config['selector']:
@@ -52,14 +42,16 @@ def handle(environ, start_response):
     store = environ['tiddlyweb.store']
     username = environ['tiddlyweb.usersign']['name']
 
-    if query.get('make'):
-        consumer = oauth.OAuthConsumer(twitter.CONSUMER_KEY,
-                twitter.CONSUMER_SECRET)
+    if query.get('make') and query.get('service'):
+        service_type = query.get('service')[0]
+        service = get_service(store, service_type)
+        consumer = oauth.OAuthConsumer(service['consumer_key'],
+                service['consumer_secret'])
 
         request = oauth.OAuthRequest.from_consumer_and_token(
                 consumer,
-                callback=_callback_url(environ),
-                http_url=twitter.REQUEST_TOKEN_URL)
+                callback=_callback_url(environ, service),
+                http_url=service['request_token_url'])
 
         request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(),
                 consumer,
@@ -68,10 +60,10 @@ def handle(environ, start_response):
         response = urllib2.urlopen(request_url)
         token = oauth.OAuthToken.from_string(response.read())
 
-        set_user_token(store, 'request_token', token, username)
+        set_user_token(store, 'request_token', token, username, service)
 
         oauth_request = oauth.OAuthRequest.from_token_and_callback(
-                token=token, http_url=twitter.AUTHORIZATION_URL)
+                token=token, http_url=service['authorization_url'])
         raise HTTP302(oauth_request.to_url())
 
     # the post was made oddly. do we care?
@@ -87,54 +79,58 @@ def send(environ, start_response):
 
     username = environ['tiddlyweb.usersign']['name']
 
-    consumer = oauth.OAuthConsumer(twitter.CONSUMER_KEY,
-            twitter.CONSUMER_SECRET)
+    service_type = query.get('service', [None])[0]
+    service = get_service(store, service_type)
 
-    access_token = get_user_token(store, 'access_token', username)
+    if service:
+        access_token = get_user_token(store, 'access_token', username, service)
 
-    if query.get('oauth_token'):
-        request_token_string = get_user_token(store, 'request_token', username)
-        if request_token_string == None:
-            # we've reached this stage in weirdness
-            raise HTTP302(_callback_url(environ))
-        request_token = oauth.OAuthToken.from_string(request_token_string)
+        consumer = oauth.OAuthConsumer(service['consumer_key'],
+                service['consumer_secret'])
 
-        query_oauth_token = environ['tiddlyweb.query']['oauth_token'][0]
-        query_oauth_verifier = environ['tiddlyweb.query']['oauth_verifier'][0]
+        if query.get('oauth_token'):
+            request_token_string = get_user_token(store, 'request_token', username, service)
+            if request_token_string == None:
+                # we've reached this stage in weirdness
+                raise HTTP302(_base_url(environ))
+            request_token = oauth.OAuthToken.from_string(request_token_string)
 
-        assert query_oauth_token == request_token.key
+            query_oauth_token = query['oauth_token'][0]
+            query_oauth_verifier = query['oauth_verifier'][0]
 
-        request = oauth.OAuthRequest.from_consumer_and_token(
-                consumer,
-                token=request_token,
-                verifier=query_oauth_verifier,
-                http_url=twitter.ACCESS_TOKEN_URL)
-        request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(),
-                consumer,
-                request_token)
+            assert query_oauth_token == request_token.key
 
-        request_url = request.to_url()
-        response = urllib2.urlopen(request_url)
-        token = oauth.OAuthToken.from_string(response.read())
+            request = oauth.OAuthRequest.from_consumer_and_token(
+                    consumer,
+                    token=request_token,
+                    verifier=query_oauth_verifier,
+                    http_url=service['access_token_url'])
+            request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(),
+                    consumer,
+                    request_token)
 
-        set_user_token(store, 'access_token', token, username)
-        raise HTTP302(_callback_url(environ))
-    elif access_token:
-        access_token = oauth.OAuthToken.from_string(access_token)
+            request_url = request.to_url()
+            response = urllib2.urlopen(request_url)
+            token = oauth.OAuthToken.from_string(response.read())
 
-        request = oauth.OAuthRequest.from_consumer_and_token(
-                consumer,
-                token=access_token,
-                http_url='http://twitter.com/statuses/user_timeline.json')
-        request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(),
-                consumer,
-                access_token)
+            set_user_token(store, 'access_token', token, username, service)
+            raise HTTP302(_base_url(environ))
+        elif access_token:
+            access_token = oauth.OAuthToken.from_string(access_token)
 
-        url = request.to_url()
-        response = urllib2.urlopen(url)
+            request = oauth.OAuthRequest.from_consumer_and_token(
+                    consumer,
+                    token=access_token,
+                    http_url=TWITTER_GET)
+            request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(),
+                    consumer,
+                    access_token)
 
-        tweets = simplejson.loads(response.read())
-        return _send_template(environ, 'tweetbag.html', {'tweets': tweets}) 
+            url = request.to_url()
+            response = urllib2.urlopen(url)
+
+            tweets = simplejson.loads(response.read())
+            return _send_template(environ, 'tweetbag.html', {'tweets': tweets}) 
     else:
         return _send_template(environ, 'tweetbag.html') 
 
@@ -152,14 +148,32 @@ def _send_template(environ, template_name, template_data=None):
     return template.generate(template_defaults)
 
 
-def _callback_url(environ):
+def _base_url(environ):
     return server_base_url(environ) + '/tweetbag'
 
 
-def get_user_token(store, token_name, username):
+def _callback_url(environ, service):
+    return _base_url(environ) + '?service=%s' % service['name']
+
+
+def get_service(store, service_name):
+    if not service_name:
+        return None
+    service_tiddler_title = 'service_%s' % service_name
+    tiddler = Tiddler(service_tiddler_title, OAUTH_BAG)
+    print tiddler
+    try:
+        tiddler = store.get(tiddler)
+        tiddler.fields['name'] = service_name
+        print tiddler.fields
+        return tiddler.fields
+    except NoTiddlerError:
+        return None
+
+def get_user_token(store, token_name, username, service):
     ensure_bag(OAUTH_BAG, store, OAUTH_BAG_POLICY,
             description='oauth info', owner=OAUTH_BAG_OWNER)
-    user_tiddler_title = '%s_%s' % (SERVICE_NAME, username)
+    user_tiddler_title = '%s_%s' % (service['name'], username)
     tiddler = Tiddler(user_tiddler_title, OAUTH_BAG)
     print 'getting tiddler', tiddler
     try:
@@ -170,10 +184,10 @@ def get_user_token(store, token_name, username):
         return None
 
 
-def set_user_token(store, token_name, token, username):
+def set_user_token(store, token_name, token, username, service):
     ensure_bag(OAUTH_BAG, store, OAUTH_BAG_POLICY,
             description='oauth info', owner=OAUTH_BAG_OWNER)
-    user_tiddler_title = '%s_%s' % (SERVICE_NAME, username)
+    user_tiddler_title = '%s_%s' % (service['name'], username)
     tiddler = Tiddler(user_tiddler_title, OAUTH_BAG)
     try:
         tiddler = store.get(tiddler)
